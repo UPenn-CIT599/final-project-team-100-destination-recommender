7b9a9bec79717b5709a1ca959d46c81e65c2bcf4
sbivins94 <bivins.sean7@gmail.com>
2020-04-06 21:06:55 -0500

    Removed the DestinationRecommenderRunner file that wasn't .java and created a new one with same name

---
+++ src/DestinationRecommenderRunner.java
@@ -0,0 +1,11 @@
+public class DestinationRecommenderRunner {
+
+    /**
+     * Main method where the Destination Recommender runs
+     *
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
