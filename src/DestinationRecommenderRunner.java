24ed7ff891383eb3444c7ce1cf1d18cd4c6cde98
ryanng14 <62918920+ryanng14@users.noreply.github.com>
2020-04-06 23:02:10 -0400

    Update DestinationRecommenderRunner.java

--- src/DestinationRecommenderRunner.java
+++ src/DestinationRecommenderRunner.java
@@ -4,8 +4,15 @@ public class DestinationRecommenderRunner {
      * Main method where the Destination Recommender runs
      *
      */
-    public static void main(String[] args) {
-
-    }
+	public static void main(String[] args) {
+		
+		SiteScoreCalc siteAnalysis = new SiteScoreCalc(SiteReader.readCSV());
+		SiteReader.readCSV();
+		System.out.println(siteAnalysis.getCountryWithMostSites(5));
+        
+        // instance of DestinationRecommender class
+        // DestinationRecommender DR = new DestinationRecommender();
+		
+	}
 
 }
