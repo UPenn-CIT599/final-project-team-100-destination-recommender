f6e146bbaa24ed7f82632ee3058b0f4480a4989c
Madhav Vohra <44486782+madhavvohra@users.noreply.github.com>
2020-04-06 17:59:19 -0700

    Included Sites csv reader

--- src/FileReader.java
+++ src/FileReader.java
@@ -57,16 +57,38 @@ public class FileReader {
 	}
 	
 	/**
-	 * Reads in a CSV of cost data with informatiion about different world heritage sites and
+	 * Reads in a CSV of cost data with information about different world heritage sites and
 	 * stores the information in an ArrayList
 	 * 
 	 * @param fileName
 	 * @return
 	 */
-	public static ArrayList<Site> siteFileReader(String fileName) {
-
-		ArrayList<Site> site = new ArrayList<Site>();
+	public static ArrayList<Site> readCSV() {
+		ArrayList<Site> sites = new ArrayList<Site>();
+		File siteFile = new File("whc-sites-2019_cleanedUp.csv");
 		
-		return site;
+		try {
+			Scanner fileReader = new Scanner(siteFile);
+			fileReader.nextLine();
+			while (fileReader.hasNextLine()) {
+				String eachLine = fileReader.nextLine();
+				String[] lineComponents = eachLine.split(",");
+				String name = lineComponents[0];
+				String category = lineComponents[1];
+				String country = lineComponents[2];
+				String region = lineComponents[3];
+				Site site = new Site(name, category, country, region);
+				sites.add(site);
+			}
+			
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			System.out.println("File not found!");
+			e.printStackTrace();
+		}
+		
+		return sites;
+		
 	}
 }
