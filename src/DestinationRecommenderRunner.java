967345d7793cbea49ea60601665a3fbd766b02d9
madhavvohra <mvohra@seas.upenn.edu>
2020-04-18 17:56:35 -0700

    Updated WeatherScoreCalc

--- src/DestinationRecommenderRunner.java
+++ src/DestinationRecommenderRunner.java
@@ -6,12 +6,18 @@ public class DestinationRecommenderRunner {
      */
 	public static void main(String[] args) {
 		
+		
 		SiteScoreCalc siteAnalysis = new SiteScoreCalc(FileReader.readCSV());
 		FileReader.readCSV();
 		System.out.println(siteAnalysis.getCountryWithMostSites(5));
         
         // instance of DestinationRecommender class
         // DestinationRecommender DR = new DestinationRecommender();
+		
+		WeatherScoreCalc dataAnalysis = new WeatherScoreCalc(FileReader.readDataCSV());
+		
+		System.out.println(dataAnalysis.WeatherList(FileReader.readDataCSV(), 75, "May"));
+		
 		
 	}
 
--- src/WeatherScoreCalc.java
+++ src/WeatherScoreCalc.java
@@ -1,17 +1,19 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 
 public class WeatherScoreCalc {
 
-	ArrayList<Weather> weather = new ArrayList<Weather>();
+	ArrayList<Data> weather = new ArrayList<Data>();
 
 	/**
 	 * This is the constructor
 	 * 
-	 * @param weather
+	 * @param arrayList
 	 */
-	public WeatherScoreCalc(ArrayList<Weather> weather) {
-		this.weather = weather;
+	public WeatherScoreCalc(ArrayList<Data> arrayList) {
+		this.weather = arrayList;
 
 	}
 
@@ -20,7 +22,7 @@ public class WeatherScoreCalc {
 	 * 
 	 * @return
 	 */
-	public ArrayList<Weather> getWeather() {
+	public ArrayList<Data> getWeather() {
 		return weather;
 	}
 
@@ -29,11 +31,68 @@ public class WeatherScoreCalc {
 	 * @param weather
 	 * @return
 	 */
-	public HashMap<String, Double> WeatherScore(ArrayList<Weather> weather) {
+	public HashMap<String, Double> WeatherScore(ArrayList<Data> data) {
 
 		HashMap<String, Double> weatherScore = new HashMap<String, Double>();
+		
+		
 
 		return weatherScore;
+	}
+	
+	public ArrayList<String> WeatherList(ArrayList<Data> lines, double tempPreference, String monthPreference) {
+		
+		HashMap <String, Double> weatherDifference = new HashMap <String, Double>();
+
+		
+		for (Data line : lines) {
+			
+			double difference = 0;
+			
+			if (monthPreference.equals("January")) {
+				difference = tempPreference - line.getJanTemp();
+			}
+			else if (monthPreference.equals("February")) {
+				difference = tempPreference - line.getFebTemp();
+			}
+			else if (monthPreference.equals("March")) {
+				difference = tempPreference - line.getMarchTemp();
+			}
+			else if (monthPreference.equals("April")) {
+				difference = tempPreference - line.getAprilTemp();
+			}
+			else if (monthPreference.equals("May")) {
+				difference = tempPreference - line.getMayTemp();
+			}
+			else if (monthPreference.equals("June")) {
+				difference = tempPreference - line.getJuneTemp();
+			}
+			else if (monthPreference.equals("July")) {
+				difference = tempPreference - line.getJulyTemp();
+			}
+			else if (monthPreference.equals("August")) {
+				difference = tempPreference - line.getAugTemp();
+			}
+			else if (monthPreference.equals("September")) {
+				difference = tempPreference - line.getSeptTemp();
+			}
+			else if (monthPreference.equals("October")) {
+				difference = tempPreference - line.getOctTemp();
+			}
+			else if (monthPreference.equals("November")) {
+				difference = tempPreference - line.getNovTemp();
+			}
+			else {
+				difference = tempPreference - line.getDecTemp();
+			}
+			
+			double absoluteDifference = Math.abs(difference); // absolute value of the difference
+			weatherDifference.put(line.getCountry(), absoluteDifference);
+		}
+		
+		System.out.println(weatherDifference);
+		// Collections.sort((List<T>) weatherDifference);
+		return null;
 	}
 
 }
