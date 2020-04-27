ac12d97f8c31bc8a4b042a580cb543558bd3e7cd
sbivins94 <bivins.sean7@gmail.com>
2020-04-27 12:36:01 -0500

    Changed sizing in weather panel, eliminated unused code in UI, added screenshots and summary.txt

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -1,9 +1,5 @@
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
-import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
-import javax.swing.plaf.metal.DefaultMetalTheme;
-import javax.swing.plaf.metal.MetalLookAndFeel;
 
 public class UserInterface {
 
@@ -12,25 +8,7 @@ public class UserInterface {
      * @param args
      */
     public static void main(String[] args) {
-        
-//        try {
-//            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-//            MetalLookAndFeel.setCurrentTheme(new CustomTheme());
-//        } catch (ClassNotFoundException e) {
-//            // TODO Auto-generated catch block
-//            e.printStackTrace();
-//        } catch (InstantiationException e) {
-//            // TODO Auto-generated catch block
-//            e.printStackTrace();
-//        } catch (IllegalAccessException e) {
-//            // TODO Auto-generated catch block
-//            e.printStackTrace();
-//        } catch (UnsupportedLookAndFeelException e) {
-//            // TODO Auto-generated catch block
-//            e.printStackTrace();
-//        }
-        
-        
+              
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
                 JFrame frame = new MainFrame("Euro Trip Destination Recommender");
